62692f3b6c355d5d0bc68f3cccf45fc04d296dda
Eric Westphal <BonanzaEric@gmail.com>
2018-03-01 16:59:09 -0500

    Extract message goroutine.

--- bot/bot.go
+++ bot/bot.go
@@ -11,8 +11,9 @@ import (
 )
 
 const (
-	energyRate = 240 // Energy refreshes one point per 4 minutes = 240 seconds
+	energyRate = 4*60 // Energy refreshes one point per 4 minutes = 240 seconds
 	arenaEnergyRate = 144 // Arena energy refreshes one point per 2.4 minutes = 144 seconds
+	palantirRate = 4*60*60 // Palantir refreshes once per 4 hours
 )
 
 var BotID string
@@ -33,8 +34,7 @@ func Start() {
 
 	BotID = u.ID
 
-	goBot.AddHandler(arenaMessageHandler)
-	goBot.AddHandler(energyMessageHandler)
+	goBot.AddHandler(messageHandler)
 
 	err = goBot.Open()
 
@@ -46,12 +46,17 @@ func Start() {
 	fmt.Println("Bot is running!")
 }
 
-func arenaMessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	var (
-		e    int
-		err  error
+		e           int
+		err         error
+		ackMessage  string
+		doneMessage string
 	)
+
+	fmt.Println(m.ContentWithMentionsReplaced())
+
 	if m.Author.ID == BotID || !strings.HasPrefix(m.Content, config.BotPrefix) {
 		return
 	}
@@ -78,67 +83,30 @@ func arenaMessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			return
 		}
 
-		go func() {
-			e = (300-e) * arenaEnergyRate
-			hour := int(e/3600)
-			minute := int((e - 3600*hour)/60)
-			second := int(e - 3600*hour - 60*minute)
-			s.ChannelMessageSend(m.ChannelID,
-				fmt.Sprintf("Alright, @%s, I'll rattle your cage in %d:%02d:%02d.  " +
-					"Maybe I'll even let you out of it.", m.Author.Mention(), hour, minute, second))
-			time.Sleep(time.Duration(e) * time.Second)
-			_, _ = s.ChannelMessageSend(m.ChannelID,
-				fmt.Sprintf("Hey, you, %s, WAKE UP!  It's time to hit the arena, you lout.", m.Author.Mention()))
-		}()
+		e = int((299.5-float32(e)) * arenaEnergyRate)
+		hour := int(e/3600)
+		minute := int((e - 3600*hour)/60)
+		second := int(e - 3600*hour - 60*minute)
+		ackMessage = fmt.Sprintf("Alright, %s, I'll rattle your cage in %d:%02d:%02d.  " +
+				"Maybe I'll even let you out of it.", m.Author.Mention(), hour, minute, second)
+		doneMessage = fmt.Sprintf(
+			"Hey, you, %s, WAKE UP!  It's time to hit the arena, you lout.", m.Author.Mention())
+
+		go messageTimer(s, m, e, ackMessage, doneMessage)
 
 	}
 }
 
-func energyMessageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
-
-	var (
-		e, max int
-		err    error
-	)
-	if m.Author.ID == BotID {
-		return
-	}
-
-	if strings.HasPrefix(m.Content, config.BotPrefix) {
-
-		f := strings.Split(m.Content[1:len(m.Content)], " ")
-
-		if f[0] == "energy" {
-			e, err = strconv.Atoi(f[1])
-			if err != nil {
-				fmt.Println(err.Error())
-				s.ChannelMessageSend(m.ChannelID,
-					fmt.Sprintf("%s, next time I'm gonna slice you up and eat you myself if you keep talking trash to me!",
-						m.Author.Mention()))
-				return
-			}
-
-			if len(f) > 1 {
-				max, err = strconv.Atoi(f[2])
-				if err != nil {
-					fmt.Println(err.Error())
-					s.ChannelMessageSend(m.ChannelID,
-						fmt.Sprintf("%s, next time I'm gonna slice you up and feed you to my caragors if you don't give me a valid maximum energy!",
-							m.Author.Mention()))
-					return
-				}
-			} else {
-				max = 178 // Max energy for level 80
-			}
-
-			go func() {
-				e = (max-e) * arenaEnergyRate
-				s.ChannelMessageSend(m.ChannelID,
-					fmt.Sprintf("Alright, @%s, I'll rattle your cage in about %d minutes.", m.Author.Mention(), int(e/60)))
-				time.Sleep(time.Duration(e) * time.Second)
-				_, _ = s.ChannelMessageSend(m.ChannelID,
-					fmt.Sprintf("Hey, you, %s, WAKE UP!  It's time to hit the arena, you lazy bastard.", m.Author.Mention()))
-			}()
-		}
-	}
+//TODO: return a channel for resetting time
+//TODO: restrict to specific bot channel
+//TODO: timers for palantir, campaign energy, ability refresh, hourly warlord, daily warlord
+//TODO: keep a struct of user data
+//TODO: say the local time too (or UTC if user doesn't give tz info)
+//TODO: vary the messages sent
+//TODO: orc adviser
+//TODO: inscription adviser (who needs which)
+func messageTimer(s *discordgo.Session, m *discordgo.MessageCreate, e int, ackMessage string, doneMessage string) {
+	s.ChannelMessageSend(m.ChannelID, ackMessage)
+	time.Sleep(time.Duration(e) * time.Second)
+	s.ChannelMessageSend(m.ChannelID, doneMessage)
 }
